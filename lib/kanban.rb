779e947d88c8e7786b082f23b72c52e791bbd911
adammartin <ebondark.od@gmail.com>
2016-03-08 12:44:25 -0600

    Using the new board factory

--- lib/kanban.rb
+++ lib/kanban.rb
@@ -6,6 +6,7 @@ require 'trello'
 require 'rufus-scheduler'
 require 'uri'
 require_relative 'local_repository'
+require_relative 'board_factory'
 require_relative 'trello_user_factory'
 require_relative 'trello_repository'
 require_relative 'summary_controller'
@@ -17,21 +18,24 @@ configure do
   config_dir = File.join pwd, '..', 'config'
   config = YConfig.new(config_dir).parse('config.yml').freeze
   member = TrelloUserFactory.new.generate config, Trello
-  lrepo = LocalRepository.new config['datadir'], config['boards'][0], FileUtils
-  trepo = TrelloRepository.new member, config['boards'][0]
   set :config, config
-  set :sum_controller, SummaryController.new(lrepo, trepo, config['boards'][0])
+  set :boards, BoardFactory.new.construct(member, config, FileUtils)
   set :scheduler, Rufus::Scheduler.new
-  set :calculator, KanbanMetricsCalculator.new(config['boards'][0], lrepo)
 end
 
 settings.scheduler.cron settings.config['daily_cfd_schedule'] do
-  settings.sum_controller.persist_summary
+  settings.boards.each do |board| board['controller'].persist_summary end
 end
 
 get '/metrics' do
-  @graphdef = settings.sum_controller.graph_definition.to_json
-  @lead_time = TimeDataFormatter.new.format_time settings.calculator.lead_time
-  @cycle_time = TimeDataFormatter.new.format_time settings.calculator.cycle_time
+  "This address is no longer valid please use /metrics/[trello_board_name]. Where [trello_board_name] is the name of the board you wish to see."
+end
+
+get '/metrics/:board' do
+  selected = URI.unescape params['board']
+  board_config = settings.boards.select do |board| board['config']['name'] == selected end[0]
+  @graphdef = board_config['controller'].graph_definition.to_json
+  @lead_time = TimeDataFormatter.new.format_time board_config['calculator'].lead_time
+  @cycle_time = TimeDataFormatter.new.format_time board_config['calculator'].cycle_time
   erb :metrics
 end
